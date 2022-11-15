1f2d22ddf79f29d56fe7af61a9310f38f36ab4d5
WellingtonBipo <wellingtonsbipo@gmail.com>
2022-11-15 12:36:58 -0300

    project start

---
+++ lib/flutter_unity_builder.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_unity_builder;
+
+export 'src/flutter_unity_builder_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/flutter_unity_builder_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_unity_builder/flutter_unity_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
